fc112cc2b5d5ee7353334526968b74cf137471fb
lucifer-886 <126491447+lucifer-886@users.noreply.github.com>
2023-03-03 19:33:12 +0530

    Update SearchForm.js

--- src/pages/SearchForm.js
+++ src/pages/SearchForm.js
@@ -1,6 +1,33 @@
-import React from "react";
+import React,{useState} from "react";
 
 const SearchForm = () => {
+    const[departureAirport,setDepartureAirport ]= useState('Delhi');
+    const[parkingCheckIn,setParkingCheckIn ]= useState('');
+    const[parkingCheckOut,setParkingCheckOut ]= useState('');
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        console.log(departureAirport);
+        console.log(parkingCheckIn);
+        console.log(parkingCheckOut);
+    }
+
+    const parkingCheckOutHandler =(e) => {
+        const {value}=e.target;
+            setParkingCheckOut(value);
+
+    }
+    const parkingCheckInHandler =(e) => {
+        const {value}=e.target;
+            setParkingCheckIn(value);
+    }
+    const departureAirportHandler =(e) => {
+        const {value}=e.target;
+        if(value.length<8){
+            setDepartureAirport(value);
+        }
+    }
+    
     return (
         <section id="hero"
         style={{backgroundImage: 'url("assets/generic_landing.jpg")', minHeight: '500px'}}>
@@ -32,7 +59,7 @@ const SearchForm = () => {
                     <div className="options row m-0"><label className="col-12 col-xl-3 p-0 mr-xl-3 mb-2">
                             <div className="heading mb-1">Departure Airport</div>
                             <div className="placeholder placeholder-airport">
-                                <input type="text" placeholder="Departure Airport" className="placeholder placeholder-airport"/>
+                                <input type="text" placeholder="Departure Airport" onChange={departureAirportHandler} value={departureAirport} className="placeholder placeholder-airport"/>
                             </div> <i
                                 className="fas fa-map-marker-alt input-icon"></i>
                         </label>
@@ -40,16 +67,16 @@ const SearchForm = () => {
                                 className="col-sm-6 p-0 pr-sm-3 date_input">
                                 <div className="heading mb-1">Parking Check-In</div>
                                 <div className="placeholder">
-                                    <input name="checkin" type="date" placeholder="Parking Check-Out" className="placeholder placeholder-airport" style={{width:"100%"}}/>
+                                    <input name="checkin" type="date" placeholder="Parking Check-Out" onChange={parkingCheckInHandler} value={parkingCheckIn} className="placeholder placeholder-airport" style={{width:"100%"}}/>
                                 </div> 
                             </label> <label className="col-sm-6 p-0 pl-sm-0 date_input">
                                 <div className="heading mb-1">Parking Check-Out</div>
-                                    <input name="Check-Out" type="date" placeholder="Parking Check-Out" className="placeholder placeholder-airport" style={{width:"100%"}}/>
+                                    <input name="Check-Out" type="date" placeholder="Parking Check-Out" onChange={parkingCheckOutHandler} value={parkingCheckOut} className="placeholder placeholder-airport" style={{width:"100%"}}/>
                                
                             </label></div>
                         <div className="col-12 col-xl-2 p-0 pl-xl-3 my-3 my-xl-0">
                             <div className="d-none d-xl-block heading mb-1 invisible">Submit</div>
-                            <button type="submit"className="btn btn-secondary btn-big btn-block p-2"><span>SEARCH</span></button>
+                            <button type="submit" onClick={onSubmitHandler} className="btn btn-secondary btn-big btn-block p-2"><span>SEARCH</span></button>
                         </div>
                     </div>
                 </form>
