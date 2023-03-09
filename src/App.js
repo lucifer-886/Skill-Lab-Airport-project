c47a655a00d7e11f5b33697233aed017177a9b9f
lucifer-886 <126491447+lucifer-886@users.noreply.github.com>
2023-03-09 18:08:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './style.css';
 import HomePage from './pages/HomePage';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Layout from './pages/Layout';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <HomePage />
+    <BrowserRouter >
+    <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<HomePage />} />
+      <Route path="login" element={<Login />} />
+      <Route path="*" element={<HomePage />} />
+    </Route>
+  </Routes>
+  </BrowserRouter >
   );
 }
 
