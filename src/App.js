af1b333f9a9d3388cc59f58a4185e91642a99827
lucifer-886 <126491447+lucifer-886@users.noreply.github.com>
2023-03-14 08:11:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from './pages/HomePage';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Layout from './pages/Layout';
 import Login from './pages/Login';
+import AirportAvailability from './pages/AirportAvailability'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <Routes>
     <Route path="/" element={<Layout />}>
       <Route index element={<HomePage />} />
+      <Route path="results" element={<AirportAvailability />} />
       <Route path="login" element={<Login />} />
       <Route path="*" element={<HomePage />} />
     </Route>
